a3c10596849320e8aca82fc126d793f29a72e743
litefit <51508317+litefit@users.noreply.github.com>
2020-07-13 10:12:29 -0400

    Updates

---
+++ precache-manifest.b2614e02fe07eab5274caa5f46df4224.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14c2afba2801144af7d116e8c8fdbb0",
+    "url": "/litefit-gh-pages/index.html"
+  },
+  {
+    "revision": "f7814deadcbe8f12b2fc",
+    "url": "/litefit-gh-pages/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "458a7ab88bad3e59c717",
+    "url": "/litefit-gh-pages/static/js/2.96471d27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/litefit-gh-pages/static/js/2.96471d27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7814deadcbe8f12b2fc",
+    "url": "/litefit-gh-pages/static/js/main.5d1f588a.chunk.js"
+  },
+  {
+    "revision": "75bb65fac4630143abe2",
+    "url": "/litefit-gh-pages/static/js/runtime-main.129671d6.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/litefit-gh-pages/static/media/logo.ee7cd8ed.svg"
+  }
+]);
